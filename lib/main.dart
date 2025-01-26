df8747fd8e9e199a7b9ea71c4709e12cd7da1d34
Ofek Udi <ofekudi97@gmail.com>
2025-01-26 09:16:51 +0200

    Change ttitle

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'UdiStreaks'),
+      home: const MyHomePage(title: 'Never Miss Twice'),
     );
   }
 }
